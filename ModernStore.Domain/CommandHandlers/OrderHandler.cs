7e831f3158a8bfd4b56d9a3697569f94fda4d5b4
Stephen Knupfer <stephenrknupfer@hotmail.com>
2018-03-28 00:00:03 -0300

    Command and Handlers

---
+++ ModernStore.Domain/CommandHandlers/OrderHandler.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidator;
+using ModernStore.Domain.Commands;
+using ModernStore.Domain.Entities;
+using ModernStore.Shared.Commands;
+
+namespace ModernStore.Domain.CommandHandlers
+{
+    public class OrderHandler : Notifiable, ICommandHandler<RegisterOrderCommand>
+    {
+        public void Handle(RegisterOrderCommand command)
+        {
+            var customer = customerRepository.Get(command.Customer);
+            var order = new Order(customer, command.DeliveryFee, command.Discount);
+
+            foreach (var item in command.Items)
+            {
+                var product = productRepository.Get(item.Product);
+                order.AddItem(new OrderItem(product, item.Quantity));
+            }
+        }
+    }
+}
